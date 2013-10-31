bf03e8c43dbe2df365daef7975aa5de51413dc8f
unknown <sujitdhamale89@gmail.com>
2013-10-31 09:19:04 +0530

    some pages added

--- contactus.php
+++ contactus.php
@@ -7,7 +7,7 @@
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
 	<link href='http://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
   </head>
-  <body id="Home">
+  <body id="contactus">
     <h1>Hello, world! My First page on Cloud</h1>
 
 	 <section class="container">
